440fbccdfc92c9a40cc434d89cf509ef8043f3c9
fernando77-M <luismoreno7700@gmail.com>
2025-04-24 15:35:50 -0500

    Add files via upload

---
+++ taller 2/design-patterns-workshop-2/src/main/java/com/example/patterns_banking/controllers/AccountController.java
@@ -0,0 +1,33 @@
+package com.example.patterns_banking.controllers;
+
+import com.example.patterns_banking.dtos.AccountDTO;
+import com.example.patterns_banking.models.Account;
+import com.example.patterns_banking.services.AccountService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+  private final AccountService accountService;
+
+  public AccountController(AccountService accountService) {
+    this.accountService = accountService;
+  }
+
+  @PostMapping
+  public Account createAccount(@RequestBody AccountDTO account) {
+    return accountService.createAccount(account);
+  }
+
+  @GetMapping("/{accountId}/deposit")
+  public Account deposit(@PathVariable Long accountId, @RequestParam Double amount) {
+    return accountService.deposit(accountId, amount);
+  }
+
+  @GetMapping("/allAccounts")
+  public List<Account> getAllAccount() {
+    return accountService.allAccount();
+  }
+}
---
+++ taller 2/design-patterns-workshop-2/src/main/java/com/example/patterns_banking/factory/AccountFactoryProvider.java
@@ -0,0 +1,33 @@
+package com.example.patterns_banking.factory;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class AccountFactoryProvider {
+  private final CheckingAccountFactory checkingAccountFactory;
+  private final SavingsAccountFactory savingsAccountFactory;
+  private final LowAmountAccountFactory lowAmountAccountFactory;
+
+  public AccountFactoryProvider(CheckingAccountFactory checkingAccountFactory, SavingsAccountFactory savingsAccountFactory, LowAmountAccountFactory lowAmountAccountFactory) {
+    this.checkingAccountFactory = checkingAccountFactory;
+    this.savingsAccountFactory = savingsAccountFactory;
+    this.lowAmountAccountFactory = lowAmountAccountFactory;
+  }
+
+  public AccountFactory getFactory(AccountType type) {
+    Map<AccountType, AccountFactory> factories = Map.of(
+      AccountType.CHECKING, checkingAccountFactory,
+      AccountType.SAVINGS, savingsAccountFactory,
+      AccountType.LOWAMOUNT, lowAmountAccountFactory);
+
+    return  factories.get(type);
+  }
+
+  public enum AccountType {
+    CHECKING,
+    SAVINGS,
+    LOWAMOUNT
+  }
+}
